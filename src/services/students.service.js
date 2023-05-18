47bb1b85549a7de37d2e6d73263aacff13ee89b9
Orlando Mendoza <ommv.17@gmail.com>
2023-05-18 10:52:50 -0400

    Creación de Query de consulta de los estudiantes con sus atributos de personas

--- src/services/students.service.js
+++ src/services/students.service.js
@@ -8,11 +8,19 @@ class StudentsService {
     // const [data, length] = await sequelize.query("SELECT * FROM dbo.tbl_estudiante")
     // const students = await Students.findAll()
     const students = await Person.findAll()
-    
+
     const people = students.map(item => `'${item.id_persona}'`).join(", ")
+
+    // const [data] = await sequelize.query(`
+    //   SELECT * FROM dbo.tbl_estudiante WHERE id_persona in (${people})
+    // `)
     
-    const [data] = await  sequelize.query(`SELECT * FROM dbo.tbl_estudiante WHERE id_persona in (${people})`)
-    
+    const [data] = await sequelize.query(`
+      SELECT * FROM dbo.tbl_estudiante
+      INNER JOIN dbo.tbl_persona
+      ON dbo.tbl_estudiante.id_persona = dbo.tbl_persona.id_persona
+    `)
+
     return {
       data,
       length: data.length,
@@ -20,12 +28,12 @@ class StudentsService {
   }
 
   async findOne(id) {
-    
+
     const person = await Person.findByPk(id)
     // const student = await Students.findOne({ where: { id_persona: id } })
-    
+
     // SELECT * FROM [db_a94371_sipi].[dbo].[tbl_estudiante] WHERE id_persona in ('CI-V-27313279', 'CI-V-11163335');
-    
+
     if (person) {
       return person
     } else {
